a2653f80b32b1a4351c3be961f25fb315a48dd0c
shivam980398 <shivayadu89@gmail.com>
2025-12-25 23:03:53 +0530

    Uploading V2 of Portfolio

---
+++ JS/script.js
@@ -0,0 +1,60 @@
+var typed = new Typed("#animated-text", {
+  strings: [
+    "Web Developer",
+    "React Developer",
+    "Backend Developer",
+    "Designer",
+    "Software Engineer",
+    "MERN Stack Developer",
+    "Flutter Developer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
+
+function downloadCV() {
+  window.open("./Assets/Resume/ShivamYadav_Resume.pdf", "_blank");
+}
+
+// for social icons display on click
+let hideTimeout;
+function toggleSocialLinks() {
+  const socialLinks = document.getElementsByClassName("social-links")[0];
+  if (socialLinks.style.display === "block") {
+    socialLinks.style.display = "none";
+  } else {
+    socialLinks.style.display = "block";
+  }
+
+  clearTimeout(hideTimeout);
+  hideTimeout = setTimeout(() => {
+    socialLinks.style.display = "none";
+  }, 4000);
+}
+
+
+// document.querySelectorAll('.card').forEach(card => { 
+//     gsap.to(card, {
+//       scale: 0.7,
+//       opacity: 0,
+//       scrollTrigger: {
+//         trigger: card,
+//         start: "top 15%",
+//         end: "bottom 15%",
+//         markers: true,
+//         scrub: true,
+//       },
+//     });
+// });
+
+// Initialize Lenis
+const lenis = new Lenis();
+
+// Use requestAnimationFrame to continuously update the scroll
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
